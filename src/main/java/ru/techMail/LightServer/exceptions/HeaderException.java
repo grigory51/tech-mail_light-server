33004f2ebd8b340f8703c625daf7fb941811d3ed
Grigory Ozhegov <grigory@ozhegov.name>
2014-09-12 17:42:07 +0400

    request exceptions

---
+++ src/main/java/ru/techMail/LightServer/exceptions/HeaderException.java
@@ -0,0 +1,10 @@
+package ru.techMail.LightServer.exceptions;
+
+/**
+ * kts, 2014
+ * Author: grigory51
+ * Date: 12/09/14
+ * Time: 11:29
+ */
+public class HeaderException extends RequestException {
+}
